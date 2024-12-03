db5735e09012a1c11747fe88b10c96a8100edb64
Abbas Ali <wwwabbasmughal786@gmail.com>
2024-12-03 10:59:08 +0500

    Update bottom_nav_bar.dart

--- lib/data/widgets/bottom_nav_bar.dart
+++ lib/data/widgets/bottom_nav_bar.dart
@@ -111,9 +111,7 @@ class _CustomBottumNavigationBarState extends State<CustomBottumNavigationBar> {
     BranchResponse response = await FlutterBranchSdk.getShortUrl(
       buo: buo,
       linkProperties: linkProperties,
-    );
-
-    // if (response.success) {
+    ); // if (response.success) {
     //   final generatedLink = response.result;
     //   content += generatedLink;
     //   Share.share(content);
